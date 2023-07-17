9ec4236836f87ec1d48ce6882cc180a5a82e29af
Balamurugan Srinivasan <balamurugan.s@altudo.co>
2023-07-17 18:34:11 +0530

    powerreview scripts

--- src/sxastarter/src/components/RecipeBanner/RecipeBanner.tsx
+++ src/sxastarter/src/components/RecipeBanner/RecipeBanner.tsx
@@ -29,6 +29,7 @@ const RecipeBanner = (): JSX.Element => {
                   <img
                     src="https://www.mccormick.com/-/media/project/oneweb/mccormick-us/frenchs/recipes/g/frenchs-green-bean-casserole-recipe.jpg?rev=9a25ddc01f14451cb27ba8fd5117d778&vd=20211118T124414Z&extension=webp&hash=12CABD4BD0BD4FEEDE462BAA5B50A59E"
                     alt="banner"
+                    loading="lazy"
                   />
                 </div>
               </div>
--- src/sxastarter/src/pages/_document.tsx
+++ src/sxastarter/src/pages/_document.tsx
@@ -10,6 +10,16 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
+          <script
+            defer
+            src="https://cdn-prod.securiti.ai/consent/auto_blocking/83bbef3c-9911-47cd-b325-8725673c2ab8/cfbaed94-437e-437f-b36c-4d67cd5254b1.js"
+          ></script>
+          <script
+            type="text/javascript"
+            src="//ui.powerreviews.com/stable/4.1/ui.js"
+            async
+            data-cookiecategory="16"
+          ></script>
         </Head>
         <body>
           <Main />
