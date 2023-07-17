7a3f40f24f50cb2fa6d7049b561591429b44c941
Balamurugan Srinivasan <balamurugan.s@altudo.co>
2023-07-17 11:31:15 +0530

    document included

--- src/sxastarter/src/pages/_app.tsx
+++ src/sxastarter/src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app';
 import { I18nProvider } from 'next-localization';
 import { SitecorePageProps } from 'lib/page-props';
+import Head from 'next/head';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 import 'assets/main.scss';
 
@@ -9,6 +13,12 @@ function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element
 
   return (
     <>
+      <Head>
+        <meta charSet="UTF-8"></meta>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="description" content="PLAY! Summit" />
+      </Head>
       <I18nProvider lngDict={dictionary} locale={pageProps.locale}>
         <Component {...rest} />
       </I18nProvider>
