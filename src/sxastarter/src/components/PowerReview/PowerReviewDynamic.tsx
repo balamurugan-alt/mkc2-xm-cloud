2128a5a4d6470c13093e482188ddc39a05d4790c
Balamurugan Srinivasan <balamurugan.s@altudo.co>
2023-07-17 20:33:32 +0530

    removing brightcode 3rd party script and powerreview

--- src/sxastarter/src/components/PowerReview/PowerReviewDynamic.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const PowerReview = dynamic(() => import('./PowerReview'), { ssr: false });
-
-export default function PowerReviewDynamic() {
-  return <PowerReview />;
-}
